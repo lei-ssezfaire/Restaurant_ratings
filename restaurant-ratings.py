01b1825a4fd6ba9e7fcceccbb5881bac7a60c5a4
Rosie <rosie@riveter13-md.swna.wdpr.disney.com>
2016-04-11 17:38:48 -0700

    Added user input for rest and rating.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -3,7 +3,7 @@ import sys
 
 def print_ratings(file_name):
 	""" Takes data file and prints ratings """
-	
+
 	try: 
 		restaurant_data = open(file_name)
 	except: 
@@ -11,15 +11,40 @@ def print_ratings(file_name):
 		sys.exit(1)
 	ratings_dict = {}
 	restaurant_names = []
+
 	for line in restaurant_data:
 		line = line.rstrip()
 		name, rating = line.split(":")
+		rating = int(rating)
 		ratings_dict[name] = rating
 		restaurant_names.append(name)
 	restaurant_names = sorted(restaurant_names)
+
 	for restaurant in restaurant_names:
 		if restaurant in ratings_dict:
 			print "{} is rated at {}.".format(restaurant, ratings_dict[restaurant])
+
+	new_restaurant = raw_input("Enter a new restaurant:\n")
+
+	while True:
+
+		try:
+			new_rating = int(raw_input("Enter restaurant rating from 1-5:\n"))
+			break
+
+		except ValueError:
+			print "Enter a whole number only."
+
+
+	ratings_dict[new_restaurant] = new_rating
+	restaurant_names.append(new_restaurant)
+	restaurant_names = sorted(restaurant_names)
+
+	for restaurant in restaurant_names:
+		if restaurant in ratings_dict:
+			print "{} is rated at {}.".format(restaurant, ratings_dict[restaurant])
+
+
 
 print_ratings(sys.argv[1])
 
