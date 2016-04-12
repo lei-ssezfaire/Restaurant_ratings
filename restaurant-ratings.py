41268425d2cbdb3926dca0bc10b92f0511c13600
Rosie <rosie@riveter13-md.swna.wdpr.disney.com>
2016-04-11 17:59:53 -0700

    Added user changing option.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,7 @@
 # your code goes here
 import sys
+import random
+
 
 def print_ratings(file_name):
 	""" Takes data file and prints ratings """
@@ -24,26 +26,36 @@ def print_ratings(file_name):
 		if restaurant in ratings_dict:
 			print "{} is rated at {}.".format(restaurant, ratings_dict[restaurant])
 
-	new_restaurant = raw_input("Enter a new restaurant:\n")
+	# new_restaurant = raw_input("Enter a new restaurant:\n")
 
-	while True:
+	# while True:
 
-		try:
-			new_rating = int(raw_input("Enter restaurant rating from 1-5:\n"))
-			break
+	# 	try:
+	# 		new_rating = int(raw_input("Enter restaurant rating from 1-5:\n"))
+	# 		break
 
-		except ValueError:
-			print "Enter a whole number only."
+	# 	except ValueError:
+	# 		print "Enter a whole number only."
 
 
-	ratings_dict[new_restaurant] = new_rating
-	restaurant_names.append(new_restaurant)
-	restaurant_names = sorted(restaurant_names)
+	# ratings_dict[new_restaurant] = new_rating
+	# restaurant_names.append(new_restaurant)
+	# restaurant_names = sorted(restaurant_names)
+
+	# for restaurant in restaurant_names:
+	# 	if restaurant in ratings_dict:
+	# 		print "{} is rated at {}.".format(restaurant, ratings_dict[restaurant])
+
+	user_name = raw_input("Hello, foodie!  What's your name?\n")
+	random_restaurant = random.choice(ratings_dict.keys())
+	print "Check out {}, it has a rating of {}".format(
+		random_restaurant, ratings_dict[restaurant])
+	user_submitted_rating = raw_input("Choose a new rating!\n")
+	ratings_dict[random_restaurant] = user_submitted_rating
 
 	for restaurant in restaurant_names:
 		if restaurant in ratings_dict:
 			print "{} is rated at {}.".format(restaurant, ratings_dict[restaurant])
-
 
 
 print_ratings(sys.argv[1])
